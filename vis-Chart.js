110f341ccfd33ced190edb0e41ae6620361d0e95
Hanzhi Yin <yinhanzhi@gmail.com>
2019-04-21 22:37:37 -0500

    first stage of finishing chart visualisation

--- vis-Chart.js
+++ vis-Chart.js
@@ -4,37 +4,73 @@
 const marginSpace = height / 4;
 
 /**
- * a helper function to put basic elements on it.
+ * Radius of each dot.
+ */
+const dotRadius = 1;
+
+/**
+ * a helper function to put basic elements on the chartSVG.
  * @param {svg} svg the main canvas to append basic elements
  * @param {Array} data the array of data that we would like to visualise
  */
 var initialiseChart = function(svg, data) {
-    var xScale = d3.scaleLinear()
+    var yearScale = d3.scaleLinear()
                     .domain([allYear[0], allYear[allYear.length - 1]])
                     .range([0, chartSVGWidth]);
     var xAxis = d3.axisBottom()
-                    .scale(xScale);
+                    .scale(yearScale);
 
-    var yScale = d3.scaleLinear()
-                    .domain([0, biggestIncomingStudent])
+    var totalStudentScale = d3.scaleLog()
+                    .base(10)
+                    .domain([1, biggestIncomingStudent])
                     .range([chartSVGHeight, 0]);
 
-    var yAxis = d3.axisLeft().scale(yScale);
+    var colourScale = studentColourScale;
 
-    var i = chartSVGMargin.top + chartSVGHeight;
+    // TODO: change the tick of the y Axis
+    var yAxis = d3.axisLeft().scale(totalStudentScale);
+                    //.ticks([1, 10, 100, 500, 10000, 30000]);
+
     svg.append("g")
+        .attr("transform", "translate(0" + "," + chartSVGHeight + ")")
         .call(xAxis);
-        //.attr("transform", "translate(0," + i + ")");
     svg.append("g")
         .call(yAxis);
+
+    allStateName.forEach(stateName => {
+        svg.append("g").attr("id", stateName).style("Visibility", "hidden");
+    })
+
+    for (let i = 0; i < data.length; i++) {
+        d3.select("#" + data[i]["State"])
+            .append("circle")
+                .attr("cx", yearScale(data[i]["Year"]))
+                .attr("cy", totalStudentScale(data[i]["Total"]))
+                .attr("r", dotRadius)
+                .attr("fill", "white")
+                .attr("stroke", colourScale(data[i]["Total"]))
+                .attr("id", data[i]["Year"] + " " + data[i]["State"]);
+        if (i < data.length - 1 && data[i]["State"] == data[i + 1]["State"]) {
+            d3.select("#" + data[i]["State"])
+                .append("line")
+                .attr("x1", yearScale(data[i]["Year"]))
+                .attr("x2", yearScale(data[i + 1]["Year"]))
+                .attr("y1", totalStudentScale(data[i]["Total"]))
+                .attr("y2", totalStudentScale(data[i + 1]["Total"]))
+                // TODO: Linear Changing colour
+                .attr("stroke", colourScale(data[i]["Total"]));
+        }
+    }
 }
 
 /**
  * the main function to append chart onto the svg
  * @param {String} state the name of the state to visualise
- * @param {svg} svg the main canvas to append chart onto
- * @param {Array} data the array of data that we would like to visualise
  */
-var drawChart = function(state, svg, data) {
-
+var changeVisibility = function(state) {
+    if (!allStateName.includes(state)) {
+        throw "invalid input";
+    }
+    var group = document.getElementById(state);
+    group.style.visibility = group.style.visibility === "visible" ? "hidden" : "visible";
 }
