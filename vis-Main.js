7947008cd96a0071d03f188cea95dc927462f527
Hanzhi Yin <yinhanzhi@gmail.com>
2019-04-23 20:18:23 -0500

    modified the svg width

--- vis-Main.js
+++ vis-Main.js
@@ -16,12 +16,12 @@ const height = 720;
 /**
  * Margin-excluded width of the map SVG canvas.
  */
-const mapSVGWidth = 900 - mapSVGMargin.left - mapSVGMargin.right;
+const mapSVGWidth = 828 - mapSVGMargin.left - mapSVGMargin.right;
 
 /**
  * Margin-excluded width of the chart SVG canvas.
  */
-const chartSVGWidth = 600 - chartSVGMargin.left - chartSVGMargin.right;
+const chartSVGWidth = 552 - chartSVGMargin.left - chartSVGMargin.right;
 
 /**
  * Margin-excluded height of the map SVG canvas.
