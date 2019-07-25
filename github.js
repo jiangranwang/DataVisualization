4066742416dea2cf1d9f21c55c7edb396c4caf5f
Hanzhi Yin <yinhanzhi@gmail.com>
2019-07-24 22:23:08 -0500

    add .gitignore; add GitHub info; github api onwork

---
+++ github.js
@@ -0,0 +1,17 @@
+'use strict';
+/**
+ * Link to the GitHub Repo
+ */
+const GitHubRepoLink = "https://github.com/int0thewind/CS-296-Project-2";
+
+/**
+ * String to present if it failed to acquire GitHub Repo info using GitHub API.
+ */
+const failureText = "Unable to acquire latest commit info. \nFor more information please directly checkout the GitHub Repo.";
+
+// let requester = new XMLHttpRequest();
+
+// requester.open("GET", "https://api.github.com/users/int0thewind");
+// console.log(requester.open);
+
+
